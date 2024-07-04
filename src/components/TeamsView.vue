dac67d8e29837c0dd3ae5716548338226a1b5643
genjy1 <idontknower1997@gmail.com>
2024-07-04 18:02:52 +0300

    some changes

--- src/components/TeamsView.vue
+++ src/components/TeamsView.vue
@@ -5,6 +5,8 @@ import { RouterLink } from 'vue-router'
 
 const teams = ref()
 let currentPage = ref(1)
+const user_id = ref()
+
 // const links = ref()
 // const link = 'https://liquipedia.net/dota2'
 
@@ -13,6 +15,16 @@ watchEffect(async () => {
     await axios
       .get(`https://gorest.co.in/public/v1/posts?page=${currentPage.value}`)
       .then((response) => (teams.value = response.data.data))
+  } catch (err) {
+    console.warn(err)
+  }
+})
+
+watchEffect(async () => {
+  try {
+    await axios
+      .get(`https://gorest.co.in/public/v2/users/${user_id.value}`)
+      .then((response) => (user.value = response.data.data))
   } catch (err) {
     console.warn(err)
   }
@@ -30,14 +42,14 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="teams__wrapper grid grid-cols-3">
+  <div class="teams__wrapper">
     <RouterLink
       :to="{ name: 'Post', params: { id: team.id } }"
       v-for="team in teams"
       :key="team.team_id"
       :id="team.id"
       :team_id="team.team_id"
-      class="flex flex-col w-96 justify-between even:border odd:bg-slate-200 p-4 rounded-xl h-80 mb-32"
+      class="flex flex-col w-full even:border odd:bg-slate-200 p-4 h-80 first:rounded-t-xl"
     >
       <h2 class="capitalize">
         {{ team.title }}
